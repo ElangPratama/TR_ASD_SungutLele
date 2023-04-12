0e1d35ad8e1c86d3704a7ebbb7401e9f01e4e0f9
ELANG pratama_ <130204498+ElangPratama@users.noreply.github.com>
2023-04-12 10:31:46 +0700

    Add files via upload

---
+++ 672022247_672022101_672022230.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+
+int main(){
+    int pilih,menu;
+
+menu:
+printf("M E N U  H O T E L\n\n");
+
+printf("1.Tambah Kamar\n");
+printf("2.LIhat Daftar Kamar\n");
+printf("3.Hapus Daftar Kamar\n");
+printf("4.Update Kamar\n");
+printf("5.Sorting Kamar\n");
+printf("6.Cari Kamar\n");
+printf("7.EXIT\n");
+
+printf("pilihanmu:");
+scanf("%d",&pilih);
+
+switch(pilih){
+
+case 1:
+
+printf("wh");
+getch();
+system("cls");
+goto menu;
+
+case 2:
+
+printf("wh");
+getch();
+system("cls");
+goto menu;
+
+
+case 3:
+
+printf("wh");
+getch();
+system("cls");
+goto menu;
+
+
+
+case 4:
+
+printf("wh");
+getch();
+system("cls");
+goto menu;
+
+
+
+case 5:
+
+printf("wh");
+getch();
+system("cls");
+goto menu;
+
+
+
+case 6:
+
+printf("wh");
+getch();
+system("cls");
+goto menu;
+
+
+
+case 7:
+system("out");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+}
